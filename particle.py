8b20da4315f2c9ea8f659c72c4cee5b5e8ba48a1
rafal <rafalsiniewicz@gmail.com>
2021-04-09 00:08:25 +0200

    update pso and particle classes

--- particle.py
+++ particle.py
@@ -1,18 +1,34 @@
-from random import uniform
-
+from random import uniform, shuffle, random
+from numpy import exp
 from base_class import Base
 
 
 class Particle(Base):
-    dimension = -1  # number of dimensions for particles
+    dimension = 0   # number of dimensions for particles
+    w = 0.5         # constant inertia weight (how much to weigh the previous velocity)
+    c1 = 1          # cognitive constant
+    c2 = 2          # social constant
+    num_features = 0
+    id = 0
 
     def __init__(self, name="PARTICLE", position=None, velocity=None):
         super().__init__(name)
-        if position:  # TODO sprawdzic czy typy sa odpowiednie, tzn. tutaj lista wartosci binarnych
+        self.id = Particle.id
+        Particle.id += 1
+        if Particle.num_features > Particle.dimension:
+            raise ValueError("Number of features to select must be lower or equal to vector dimension")
+        if position and isinstance(position, list) and all(element in [0, 1] for element in position):
             self.position = position
+        elif position and (not isinstance(position, list) or not all(element in [0, 1] for element in position)):
+            raise TypeError("Position of particles must be a list of binary values")
         else:
-            self.position = []  # wylosowac jakis wektor binarny o wymiarach dimension x 1
-        if velocity:  # TODO sprawdzic czy typy sa odpowiednie, tzn. tutaj lista wartosci float
+            # initialize position vector as binary vector with exact "num_features" of 1's and the rest numbers are 0
+            selected = [1] * Particle.num_features  # list of 1's (selected features)
+            not_selected = [0] * (Particle.dimension - Particle.num_features)  # list of 0's ( not selected features)
+            self.position = selected + not_selected
+            shuffle(self.position)
+        if velocity and isinstance(position,
+                                   list):  # TODO sprawdzic czy typy sa odpowiednie, tzn. tutaj lista wartosci float/int
             self.velocity = velocity
         else:
             self.velocity = [uniform(-1, 1) for d in range(Particle.dimension)]
@@ -20,16 +36,59 @@ class Particle(Base):
         self.best_error = -1
         self.error = -1
 
+    def __str__(self):
+        return "\n### Particle ###\nid = {}\nposition={}\nvelocity={}\nbest_position={}\nbest_error={}\nerror={}\n".format(
+            self.id, self.position, self.velocity, self.best_position, self.best_error, self.error)
 
+    def update_velocity(self, best_global_position):
+        """
+        Update particle velocity from equation:
+        v^{i}_{k+1} = w_{k}*v^{i}_{k} + c_{1}*r_{1}*(p^{i}_{k} - x^{i}_{k}) + c_{2}*r_{2}*(p^{g}_{k} - x^{i}_{k})
+        :param best_global_position:
+        :return:
+        """
+        self.log_debug("Updating particle's velocity")
+        for i in range(Particle.dimension):
+            r1 = random()
+            r2 = random()
 
-    def update_velocity(self):
-        pass
-
+            vel_cognitive = Particle.c1 * r1 * (self.best_position[i] - self.position[i])
+            vel_social = Particle.c2 * r2 * (best_global_position[i] - self.position[i])
+            self.velocity[i] = Particle.w * self.velocity[i] + vel_cognitive + vel_social
 
     def update_position(self):
-        pass
+        """
+        Update particle position.
+        :return:
+        """
+        self.log_debug("Updating particle's position")
+        for i in range(Particle.dimension):
+            if random() < self.sigmoid(self.velocity[i]):
+                self.position[i] = 1
+            else:
+                self.position[i] = 0
 
     def evaluate(self):
+        self.error = self.cost_function()
+
+        # check to see if the current position is an individual best
+        if self.error < self.best_error or self.best_error == -1:
+            self.best_position = self.position
+            self.best_error = self.error
+
+    @staticmethod
+    def sigmoid(x):
+        z = exp(-x)
+        sig = 1 / (1 + z)
+
+        return sig
+
+    def cost_function(self):
+        """
+        Calculate cost funtion value for particle.
+        :return:
+        """
+        MAD = 0
+        # TODO implement MAD as a cost function
         pass
-
-
+        return MAD
--- pso.py
+++ pso.py
@@ -4,7 +4,7 @@ from base_class import Base
 from particle import Particle
 
 class PSO(Base):
-    def __init__(self, name="PSO", dimension=-1, num_features=3, max_iter=100):
+    def __init__(self, name="PSO", dimension=-1, num_features=3, max_iter=100, num_particles=10):
         super().__init__(name)
         self.dimension: int = dimension                    # number of dimensions for each particle
         self.num_features: int = num_features              # number of features to select
@@ -12,18 +12,45 @@ class PSO(Base):
         self.swarm: List[Particle] = []                    # list of particles (swarm)
         self.best_global_position: List[int] = []          # best position of all particles in swarm
         self.best_global_error: float = -1                 # best error of all particles in swarm
+        self.num_particles: int = num_particles
+
+
+    def set_particles_parameters(self, w=0.5, c1=1, c2=2):
+        """
+        Set parameters for all particles.
+        :param w:
+        :param c1:
+        :param c2:
+        :return:
+        """
+        Particle.dimension = self.dimension
+        Particle.num_features = self.num_features
+        Particle.w = w
+        Particle.c1 = c1
+        Particle.c2 = c2
+
 
     def initialize_swarm(self):
         """
-        Initialize swarm of paricles
+        Initialize swarm of particles
+        :return self.swarm (list[Particle]):                swarm of particles
+        """
+        self.set_particles_parameters()
+        self.swarm = [Particle() for d in range(self.num_particles)]
+        return self.swarm
+
+    def run(self):
+        """
+        Run PSO algorithm.
         :return:
         """
         pass
 
-    def run(self):
-        """
-        Run PSO algorithm
-        :return:
-        """
-        pass
+
+
+if __name__ == "__main__":
+    pso = PSO(dimension=10)
+    pso.initialize_swarm()
+    for i in pso.swarm:
+        print(i)
 
